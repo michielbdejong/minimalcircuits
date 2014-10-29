3bc0807800d70a1f2b0dedc4719044d25d6d889c
Michiel de Jong <michiel@unhosted.org>
2014-10-29 11:20:38 +0000

    draft 14 cases

--- kmaps.js
+++ kmaps.js
@@ -14,14 +14,16 @@ var cases = {};
 function setCases() {
   var i, str, str1, str2, strs;
 
-  //case 1: one 1
+  //case 1: one 1 -> straight(3)
+  //rowOrBlock('0001', '0010', '0100', '1000')
   str = '00000001';
   for (i=0; i<8; i++) {    
     cases[str] = 1;
     str = str.substring(1)+str[0];
   }
 
-  //case 2: two 1's, differing in 1 var
+  //case 2: two 1's, differing in 1 var -> straight(2)
+  //rowOrBlock('1100', '0110', '0011', '1001')
   var strs = ['1100', '0110', '0011', '1001'];
   for (i=0; i<strs.length; i++) {
     cases[strs[i]+'0000'] = 2;
@@ -33,7 +35,8 @@ function setCases() {
     str = str.substring(1)+str[0];
   }
 
-  //case 3: two 1's, differing in 2 vars
+  //case 3: two 1's, differing in 2 vars -> const+straight(1)
+  //rowOrBlock('1010', '0101')
   strs = ['1010', '0101'];
   for (i=0; i<strs.length; i++) {
     cases[strs[i]+'0000'] = 3;
@@ -48,12 +51,42 @@ function setCases() {
     str2 = str2.substring(1)+str2[0];
   }
 
-  //case 4: two 1's, differing in 3 vars
+  //case 4: two 1's, differing in 3 vars -> 2x straight(3)
+  //threeVarSplit('1', '1')
   cases['10000010']=4;
   cases['01000001']=4;
   cases['00101000']=4;
   cases['00010100']=4;
 
+  //case 5: three 1's, adjacant
+  //rowOrBlock('1110', '1101', '1011', '0111')
+
+  //case 6: three 1's, two of them adjacent
+  //twoVarSplit('11', ['01', '10'])
+
+  //case 7: three 1's, none adjacent -> 3x straight(3)
+  //threeQuarterRowOrBlock('100110')
+
+  //case 8: four 1's, all adjacent
+  //rowOrBlock('1111')
+
+  //case 9: four 1's, three of them adjacent
+  //oneVarSplit(['1110', '1101', '1011', '0111'], ['0001', '0010', '0100', '1000'])
+
+  //case 10: four 1's, snake-like-adjacent
+  //threeQuarterRowOrBlock('011110')
+
+  //case 11: four 1's, wedge-like-adjacent
+  //threeQuarterRowOrBlock('011101')
+
+  //case 12: four 1's, adjacent in two pairs
+  //twoVarSplit('11', '11')
+
+  //case 13: four 1's, none adjacent -> xor
+  //xor
+
+  //case 14: eight 1's
+  cases['11111111']=14;
 }
 
 function printKMaps(numVars, numCols) {
